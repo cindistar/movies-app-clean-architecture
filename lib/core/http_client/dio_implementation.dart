bba1ecbe15a70e76958352e6094c2886a30ea0f9
Cindi Nishimoto <cindi_nishi@hotmail.com>
2022-01-19 13:43:41 -0300

    deleted api key for safety reasons

--- lib/core/http_client/dio_implementation.dart
+++ lib/core/http_client/dio_implementation.dart
@@ -9,11 +9,10 @@ class DioHttpClientImplementation implements HttpClient {
         BaseOptions(
           baseUrl: "https://api.themoviedb.org/3",
           headers: {
-            'api-key': 'api_key=d20a9f45fadf53b679cdb601f6f98125',
+            'api-key': '',
             'contentType': 'application/json; charset=utf-8',
             'language': 'en-US',
-            'authorization':
-                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjBhOWY0NWZhZGY1M2I2NzljZGI2MDFmNmY5ODEyNSIsInN1YiI6IjYxODk4YmRkMTYwZTczMDA2NzU0MmQ0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lc4T9V-qce-g841Nn2UN8nWVBPMF9EQzYYcUFNQuvlU'
+            'authorization': 'Bearer '
           },
         ),
       );
