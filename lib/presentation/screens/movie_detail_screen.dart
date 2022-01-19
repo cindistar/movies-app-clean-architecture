4424167cea19bf23b3d55457e9d49fd09ccf5b52
Cindi Nishimoto <cindi_nishi@hotmail.com>
2022-01-18 21:12:45 -0300

    commit

--- lib/presentation/screens/movie_detail_screen.dart
+++ lib/presentation/screens/movie_detail_screen.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:intl/intl.dart';
+
 
 class MovieDetailScreen extends StatefulWidget {
   final MovieDetailArguments movieDetailArguments;
